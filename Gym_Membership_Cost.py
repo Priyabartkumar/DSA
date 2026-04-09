13c2d3763c015878d814741bc7c5b04470317aea
Priyabartkumar <122349044+Priyabartkumar@users.noreply.github.com>
2026-04-09 23:50:42 +0530

    Update Gym_Membership_Cost.py

--- Gym_Membership_Cost.py
+++ Gym_Membership_Cost.py
@@ -1,12 +1,14 @@
-A gym offers membership plans based on the number of months a customer enrolls. Write a program that prints the total membership cost or an appropriate message for edge cases.
+Q) A gym offers membership plans based on the number of months a customer enrolls. Write a program that prints the total membership cost or an appropriate message for edge cases.
 
 📐 Pricing Rules
-Duration	Cost (₹)
-1 month	₹2,000
-2 or 3 months	₹5,000
-4 to 6 months	₹9,000
-9 months	₹12,000
-12 months	₹15,000
+Duration	                Cost (₹)
+1 month	                  ₹2,000
+2 or 3 months	            ₹5,000
+4 to 6 months	            ₹9,000
+9 months	                ₹12,000
+12 months	                ₹15,000
+
+
 Input < 0	invalid input
 Input = 0	0
-Any other value	Error
+
