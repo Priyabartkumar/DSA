a62051d4b80e28f5a64db91af9bb17cfd018135a
Priyabartkumar <122349044+Priyabartkumar@users.noreply.github.com>
2026-07-13 23:59:33 +0530

    Update Merged K Sorted List.java

--- Merged K Sorted List.java
+++ Merged K Sorted List.java
@@ -1,3 +1,4 @@
+QUestion n.o 23
 You are given an array of k linked-lists lists, each linked-list is sorted in ascending order.
 
 Merge all the linked-lists into one sorted linked-list and return it.
