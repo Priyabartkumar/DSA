90f764699ade3c9eb7f28ee2c85d88b32f9f6f76
Priyabartkumar <122349044+Priyabartkumar@users.noreply.github.com>
2026-07-14 23:58:38 +0530

    Revise problem description in Merged K Sorted List
    Updated problem statement and example for clarity.

--- Merged K Sorted List.java
+++ Merged K Sorted List.java
@@ -1,15 +1,9 @@
 Level Hard
 Question:23
 
+You are given an array of k linked-lists lists, each linked-list is sorted in ascending order. Merge all the linked-lists into one sorted linked-list and return it.
 
- 
-You are given an array of k linked-lists lists, each linked-list is sorted in ascending order.
-
-Merge all the linked-lists into one sorted linked-list and return it.
-
- 
-
-Example 1:
+ Example 1:
 
 Input: lists = [[1,4,5],[1,3,4],[2,6]]
 Output: [1,1,2,3,4,4,5,6]
