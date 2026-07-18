160cd79dd9af49551fff7d5bb9f50d36801bfb1f
Priyabartkumar <122349044+Priyabartkumar@users.noreply.github.com>
2026-07-18 19:17:31 +0530

    Update Merged K Sorted List.java

--- Merged K Sorted List.java
+++ Merged K Sorted List.java
@@ -46,5 +46,14 @@ class Solution {
                 ln = ln.next;
             }
         }
+     Collections.sort(l);
+
+        ListNode head = new ListNode(0);
+        ListNode h = head;
+        for (int i:l){
+            ListNode t = new ListNode(i);
+            h.next =t;
+            h = h.next;
+        }
      
 
