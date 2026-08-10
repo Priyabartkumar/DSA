dda1e960bce677eb368c5fc60446b7f46541852b
Priyabartkumar <122349044+Priyabartkumar@users.noreply.github.com>
2026-08-11 04:18:08 +0530

    Update Find First and Last Position of Element in Sorted Array.java

--- Find First and Last Position of Element in Sorted Array.java
+++ Find First and Last Position of Element in Sorted Array.java
@@ -2,7 +2,7 @@ Level:Medium
 Question n.o 34:
 Topic:Binary Search,Array
 Time Complexity:O(log n)
-Spce Cmplexity:
+Spce Cmplexity:O(1)
  
 Given an array of integers nums sorted in non-decreasing order, find the starting and ending position of a given target value.
 
